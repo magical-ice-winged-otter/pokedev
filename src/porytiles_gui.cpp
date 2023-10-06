820e035bebdebb6d07fed7dddf104b36817d3ca5
Daniel Walls <walls.dk@gmail.com>
2023-10-05 17:46:52 -0700

    nicer gui, preparing for more settings

--- src/porytiles_gui.cpp
+++ src/porytiles_gui.cpp
@@ -1,4 +1,3 @@
-#include <cstdio>
 #include <imgui.h>
 #include "file_dialog.hpp"
 #include "porytiles_gui.hpp"
@@ -8,28 +7,80 @@ using namespace std;
 
 namespace PorytilesGui
 {
+    // non-serialized
+    static string s_paletteMode; // options: 'true-color' or 'greyscale'
+    // base game - enum
+    // dual layer - bool
+    // transparency color R, G, and B - color
+    // default behavior - string
+
+    // General Settings
     static filesystem::path s_projectPath {};
     static filesystem::path s_behaviorsHeaderPath {};
-    static int s_currentMode {};
+    static filesystem::path s_outputPath {};
+
+    // Tools
     static bool s_showPrimaryCompilerTool {};
     static bool s_showPrimaryDecompilerTool {};
     static bool s_showSecondaryDecompilerTool {};
     static bool s_showSecondaryCompilerTool {};
 
+    static filesystem::path s_sourcePrimaryPath {};
+    static filesystem::path s_sourceSecondaryPath {};
+    static filesystem::path s_sourcePartnerPrimaryPath {};
+    static filesystem::path s_compiledPrimaryPath {};
+    static filesystem::path s_compiledSecondaryPath {};
+    static filesystem::path s_compiledPartnerPrimaryPath {};
+
     void init()
     {
         Serializer::registerValue("projectPath", s_projectPath);
         Serializer::registerValue("behaviorsHeaderPath", s_behaviorsHeaderPath);
-        Serializer::registerValue("currentMode", s_currentMode);
         Serializer::registerValue("showPrimaryCompiler", s_showPrimaryCompilerTool);
         Serializer::registerValue("showPrimaryDecompiler", s_showPrimaryDecompilerTool);
         Serializer::registerValue("showSecondaryCompiler", s_showSecondaryCompilerTool);
         Serializer::registerValue("showSecondaryDecompiler", s_showSecondaryDecompilerTool);
+        Serializer::registerValue("outputPath", s_outputPath);
     }
 
     void shutdown()
     {
         // No shutdown logic needed for now.
+    }
+
+    static ImVec4 s_subTextColor {0.6, 0.6, 0.6, 1};
+    static ImVec4 s_errorTextColor {1.0, 0.3, 0.3, 1};
+
+    static void ImGuiFolderPicker(const char* label, filesystem::path& path)
+    {
+        if (ImGui::Button("Edit"))
+            FileDialog::tryPickFolder(path);
+
+        ImGui::SameLine();
+        ImGui::Text("%s", label);
+
+        if (path.string().empty())
+            ImGui::TextColored(s_errorTextColor, "No folder selected");
+
+        else ImGui::TextColored(s_subTextColor, "%s", path.string().c_str());
+
+        ImGui::Spacing(); ImGui::Spacing();
+    }
+
+    static void ImGuiFilePicker(const char* label, filesystem::path& path, const char* filter = nullptr)
+    {
+        if (ImGui::Button("Edit"))
+            FileDialog::tryPickFile(path, filter);
+
+        ImGui::SameLine();
+        ImGui::Text("%s", label);
+
+        if (path.string().empty())
+            ImGui::TextColored(s_errorTextColor, "No file selected");
+
+        else ImGui::TextColored(s_subTextColor, "%s", path.string().c_str());
+
+        ImGui::Spacing(); ImGui::Spacing();
     }
 
     void render()
@@ -39,57 +90,106 @@ namespace PorytilesGui
         ImGui::SetNextWindowSize(viewport->Size);
         ImGui::Begin("Main Window", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_MenuBar);
 
-        if (ImGui::BeginMenuBar())
+        // Menu Bar
         {
-            if (ImGui::BeginMenu("Tools"))
+            if (ImGui::BeginMenuBar())
             {
-                ImGui::MenuItem("Primary Compiler", nullptr, &s_showPrimaryCompilerTool);
-                ImGui::MenuItem("Primary Decompiler", nullptr, &s_showPrimaryDecompilerTool);
-                ImGui::MenuItem("Secondary Compiler", nullptr, &s_showSecondaryCompilerTool);
-                ImGui::MenuItem("Secondary Decompiler", nullptr, &s_showSecondaryDecompilerTool);
-                ImGui::EndMenu();
+                if (ImGui::BeginMenu("Tools"))
+                {
+                    ImGui::MenuItem("Primary Compiler", nullptr, &s_showPrimaryCompilerTool);
+                    ImGui::MenuItem("Primary Decompiler", nullptr, &s_showPrimaryDecompilerTool);
+                    ImGui::MenuItem("Secondary Compiler", nullptr, &s_showSecondaryCompilerTool);
+                    ImGui::MenuItem("Secondary Decompiler", nullptr, &s_showSecondaryDecompilerTool);
+                    ImGui::EndMenu();
+                }
+
+                ImGui::EndMenuBar();
             }
-
-            ImGui::EndMenuBar();
         }
 
-        ImGui::ShowDemoWindow();
-
-        if (s_showPrimaryCompilerTool && ImGui::Begin("Primary Compiler", &s_showPrimaryCompilerTool))
+        // Windows
         {
-            if (ImGui::Button("Edit"))
-                FileDialog::tryPickFolder(s_projectPath);
-
-            ImGui::SameLine();
-            ImGui::Text("Source Primary Path: %s", s_projectPath.string().c_str());
-            ImGui::End();
+            ImGui::ShowDemoWindow();
+            if (s_showPrimaryCompilerTool && ImGui::Begin("Primary Compiler", &s_showPrimaryCompilerTool))
+            {
+                ImGuiFolderPicker("Source Primary Path", s_sourcePrimaryPath);
+                ImGui::SetItemTooltip("Path to a directory containing the source data for a primary set.");
+                ImGui::End();
+            }
+            if (s_showPrimaryDecompilerTool && ImGui::Begin("Primary Decompiler", &s_showPrimaryDecompilerTool))
+            {
+                ImGuiFolderPicker("Source Secondary Path", s_sourceSecondaryPath);
+                ImGuiFolderPicker("Source Partner Primary Path", s_sourceSecondaryPath);
+                ImGui::End();
+            }
+            if (s_showSecondaryCompilerTool && ImGui::Begin("Secondary Compiler", &s_showSecondaryCompilerTool))
+            {
+                ImGuiFolderPicker("Compiled Primary Path", s_compiledPrimaryPath);
+                ImGui::End();
+            }
+            if (s_showSecondaryDecompilerTool && ImGui::Begin("Secondary Decompiler", &s_showSecondaryDecompilerTool))
+            {
+                ImGuiFolderPicker("Compiled Secondary Path", s_compiledSecondaryPath);
+                ImGuiFolderPicker("Compiled Partner Primary Path", s_compiledPartnerPrimaryPath);
+                ImGui::End();
+            }
         }
-        if (s_showPrimaryDecompilerTool && ImGui::Begin("Primary Decompiler", &s_showPrimaryDecompilerTool))
+
+        // Settings
         {
-            ImGui::End();
+            ImGui::SeparatorText("Settings");
+
+            if (ImGui::CollapsingHeader("Driver"))
+            {
+                ImGui::Spacing(); ImGui::Spacing();
+                ImGui::Indent();
+                ImGuiFolderPicker("Project Path", s_projectPath);
+                ImGuiFolderPicker("Output Path", s_outputPath);
+                ImGuiFilePicker("Behaviors Header File", s_behaviorsHeaderPath, "h,hpp");
+                ImGui::Unindent();
+            }
+            if (ImGui::CollapsingHeader("Tileset (De)compilation"))
+            {
+                ImGui::Spacing(); ImGui::Spacing();
+                ImGui::Indent();
+
+                ImGui::Unindent();
+            }
+            if (ImGui::CollapsingHeader("Color assignment Config"))
+            {
+                ImGui::Spacing(); ImGui::Spacing();
+                ImGui::Indent();
+
+                ImGui::Unindent();
+            }
+            if (ImGui::CollapsingHeader("Fieldmap Override"))
+            {
+                ImGui::Spacing(); ImGui::Spacing();
+                ImGui::Indent();
+
+                ImGui::Unindent();
+            }
+            if (ImGui::CollapsingHeader("Warnings"))
+            {
+                ImGui::Spacing(); ImGui::Spacing();
+                ImGui::Indent();
+
+                ImGui::Unindent();
+            }
         }
-        if (s_showSecondaryCompilerTool && ImGui::Begin("Secondary Compiler", &s_showSecondaryCompilerTool))
+
+        // Tools
         {
-            ImGui::End();
+            ImGui::SeparatorText("Tools");
+
+            ImGui::Text("Primary"); ImGui::SameLine();
+            s_showPrimaryCompilerTool |= ImGui::Button("Compiler"); ImGui::SameLine();
+            s_showPrimaryDecompilerTool |= ImGui::Button("Decompiler");
+
+            ImGui::Text("Secondary"); ImGui::SameLine();
+            s_showSecondaryCompilerTool |= ImGui::Button("Compiler"); ImGui::SameLine();
+            s_showSecondaryDecompilerTool |= ImGui::Button("Decompiler");
         }
-        if (s_showSecondaryDecompilerTool && ImGui::Begin("Secondary Decompiler", &s_showSecondaryDecompilerTool))
-        {
-            ImGui::End();
-        }
-
-        ImGui::SeparatorText("General Settings");
-
-        if (ImGui::Button("Edit"))
-            FileDialog::tryPickFolder(s_projectPath);
-
-        ImGui::SameLine();
-        ImGui::Text("Project path: %s", s_projectPath.string().c_str());
-
-        if (ImGui::Button("Edit"))
-            FileDialog::tryPickFile(s_behaviorsHeaderPath, "h,hpp");
-
-        ImGui::SameLine();
-        ImGui::Text("Behaviors Header: %s", s_behaviorsHeaderPath.string().c_str());
 
         ImGui::End();
     }
