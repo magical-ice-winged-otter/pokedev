17162ed715a45507589b8d59484a9f3b30b675af
Daniel Walls <walls.dk@gmail.com>
2024-01-13 01:39:37 -0800

    update windows functions to return a success bool

--- src/platform/platform_windows.hpp
+++ src/platform/platform_windows.hpp
@@ -10,8 +10,8 @@ namespace Platform::Windows
     void shutdown();
     bool tryPickFile(std::filesystem::path& outPath, const FilePickerOptions& options);
     bool tryPickFolder(std::filesystem::path& outPath, const FilePickerOptions& options);
-    void openPath(const std::filesystem::path& path);
-    void openFile(const std::filesystem::path& path);
+    bool openPath(const std::filesystem::path& path);
+    bool openFile(const std::filesystem::path& path);
 }
 
 #endif // POKEDEV_PLATFORM_WINDOWS_HPP
