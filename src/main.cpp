304254a49b4d003142c3e9d7e84a5dd789feb98c
poetahto <walls.dk@gmail.com>
2024-01-30 19:19:46 -0800

    removed commented code from main

--- src/main.cpp
+++ src/main.cpp
@@ -3,20 +3,6 @@
 
 #include <fmt/core.h>
 #include <filesystem>
-#include "spritesheets/draw.hpp"
-#include "spritesheets/spritesheets.hpp"
-
-
-/*
-std::optional<uchar*> callback(DrawUtil::GeneratorPixel& genPixel) {
-    printf("channels: %d", genPixel.channels);
-    for (int c = 0; c < genPixel.channels; ++c) {
-        genPixel.pointer[c] = (genPixel.pointer[c] * 2) % 255 ;
-    }
-
-    return std::make_optional<uchar*>(genPixel.pointer);
-}
-*/
 
 int main(int argc, char* argv[]) {
     Platform::init();
